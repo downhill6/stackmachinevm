8662bec479bbb192d984881fb5fcb16e8b038759
大下坡 <sdk233@outlook.com>
2024-02-23 21:11:31 +0800

    test: 添加递归函数测试

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 void test(vector<int> &code, int val) {
-    vector<Operand> stack(40);
+    vector<Operand> stack(100);
     int init_pc = 0;
     VM machine {code, stack, init_pc};
     int res = machine.run();
@@ -24,15 +24,27 @@ int main() {
     vector<int> if_test1 { 0, 0, 8, 8, 0, 5, 9, 10, 0, 10, 10,};
     vector<int> if_test2 { 0, 1, 8, 8, 0, 5, 9, 10, 0, 10, 10,};
     vector<int> call_test1 { 6, 4, 0, 10, 0, 10, 7, 0,};
-    vector<int> call_test2 { 0, 2, 0, 3, 6, 8, 2, 10, 3, 2, 3, 3,1, 7, 2,};
-
+    vector<int> call_test2 { 6, 4, 0, 10, 0, 2, 0, 3, 6, 13, 2, 7,
+                             0, 3, 1,  3, 1, 1, 7, 2};
+    // fact(5)
+    vector<int> fact_test {6, 4,  0, 10, 0, 5, 6, 11,  1, 7, 0,  3,
+                           0, 8, 28,  3, 0, 3, 1,  0, -1, 1, 6, 11,
+                           1, 2,  9, 30, 0, 1, 7,  1};
+    // fib(7)
+    vector<int> fib_test {6,  4,  0, 10,  0, 7,  6, 11,  1,  7, 0, 3,
+                          0,  8, 45,  3,  0, 0, -1,  1,  8, 41, 3, 0,
+                          0, -1,  1,  6, 11, 1,  3,  1,  0, -2, 1, 6,
+                          11,  1,  1,  9, 43, 0,  1,  9, 47,  0, 1, 7,
+                          1, };
     test(cst_test, 1);
     test(add_test, 3);
     test(swap_test, 2);
     test(if_test1, 10);
     test(if_test2, 5);
     test(call_test1, 10);
-    test(call_test2, 4);
+    test(call_test2, 5);
+    test(fact_test, 120);
+    test(fib_test, 21);
 
     return 0;
 }
